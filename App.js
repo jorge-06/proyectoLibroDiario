c4228bac8efaa5dab5d0d6b3ef454e0009bf8e15
jorge06 <jorgevn06@gmail.com>
2023-12-06 22:44:16 -0300

    2do desafio

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react-native"
+import { View, TextInput, Button, StyleSheet, FlatList} from "react-native"
+import uuid from 'react-native-uuid'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Libro = () =>{
+
+  const [ newTitleProduct, setNewTitleProduct] = useState ("")
+  const [ newProductPrice, setNewProductPrice] = useState ("")
+  const [products, setProducts] = useState ([])
+  
+  const handlerAddProduct = () => {
+    const newproduct = {
+      id : uuid.v4(),
+      title: newTitleProduct,
+      price: newProductPrice,
+    }
+    setProducts (current => [...current,newproduct])
+    setNewTitleProduct ("")
+    setNewProductPrice ("")
+  }
+  
+
+  return <View style={estilos.fondo}>
+              <View style = {estilos.barraSup}>
+                <TextInput style = {estilos.buscador}
+                placeholder="Producto"
+                onChangeText={(t)=> setNewTitleProduct (t)}
+                />
+                <TextInput style = {estilos.buscador}
+                placeholder="Precio"
+                onChangeText={(t)=> setNewProductPrice (t)}
+                />
+                <Button title = "ADD" onPress={handlerAddProduct}/>
+              </View>
+              <View style={estilos.listContainer}>
+                <FlatList
+                data={products}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => <View style ={estilos.cardProduct}>
+                <Text>{item.title}</Text>
+                <Text>{item.price} $</Text>
+                <Button title="DEL"/>
+              </View>}
+                />
+              </View>
+              
+         </View>
+         
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const estilos = StyleSheet.create ({
+  fondo: {
+    backgroundColor : "red",
+    flex : 1,
+    justifyContent: "start",
+    alignItems: "center",
+    marginTop: "10%",
   },
-});
+  barraSup: {
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "space-around",
+   },
+   
+  buscador: {
+    borderWidth: 1,
+    backgroundColor: "white",
+    borderRadius: 5,
+    width: "30%",
+  },
+  listContainer:{
+    backgroundColor: "green",
+  },
+  cardProduct:{
+    backgroundColor: "yellow",
+  },
+
+})
+
+
+
+
+export default Libro
